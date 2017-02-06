10c50ec56e0d14a54eb1a8a363e0e8a032335f7e
Heinz Karsten Hiekmann <k.hiekmann@andagon.com>
2017-02-06 23:53:35 +0100

    green - BuzzException

--- src/main/fizzbuzz/FizzBuzz.java
+++ src/main/fizzbuzz/FizzBuzz.java
@@ -8,10 +8,15 @@ public class FizzBuzz
 
 	public long reactTo(long number) throws Exception
 	{
-		if (moduloThreeEqualsZero(number)) {
-			throw new FizzException();
-		}
+		if (moduloThreeEqualsZero(number)) { throw new FizzException();}
+		if (moduloFiveEqualsZero(number)) {throw new BuzzException();}
 		return number;
+	}
+
+	private boolean moduloFiveEqualsZero(long number)
+
+	{
+		return (number % 5) == 0;
 	}
 
 	private boolean moduloThreeEqualsZero(long number)
