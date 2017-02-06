3a1eefc9bc19649fbb0313b82fd85a0975a1c622
Heinz Karsten Hiekmann <k.hiekmann@andagon.com>
2017-02-06 23:58:58 +0100

    refactor modulo function

--- src/main/fizzbuzz/FizzBuzz.java
+++ src/main/fizzbuzz/FizzBuzz.java
@@ -9,29 +9,13 @@ public class FizzBuzz
 	public long reactTo(long number) throws Exception
 	{
 		if (moduloEqualsZero(3 * 5, number)) { throw new FizzBuzzException();}
-		if (moduloThreeEqualsZero(number)) { throw new FizzException();}
-		if (moduloFiveEqualsZero(number)) {throw new BuzzException();}
+		if (moduloEqualsZero(5, number)) {throw new BuzzException();}
+		if (moduloEqualsZero(3, number)) { throw new FizzException();}
 		return number;
 	}
 
 	private boolean moduloEqualsZero(int modulant, long number)
 	{
 		return (number % modulant) == 0;
-	}
-
-	private boolean moduloThreeAndFiveEqualsZero(long number)
-	{
-		return moduloThreeEqualsZero(number) && moduloFiveEqualsZero(number);
-	}
-
-	private boolean moduloFiveEqualsZero(long number)
-
-	{
-		return (number % 5) == 0;
-	}
-
-	private boolean moduloThreeEqualsZero(long number)
-	{
-		return (number % 3) == 0;
 	}
 }
